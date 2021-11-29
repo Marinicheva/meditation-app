6b3b5c63decd047181255cd5e2fc0068e047c9ec
Kate <marri.nich@yandex.ru>
2021-11-29 17:28:32 +0300

    Add playing rain-song

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -4,20 +4,55 @@ const playBtn = document.querySelector('#play-btn');
 const pauseBtn = document.querySelector('#pause-btn');
 const timerButtons = document.querySelector('#timer-buttons');
 const timerPanel = document.querySelector('#time-panel');
+const song = document.querySelector('#song');
 
 let time = 10;
+let timerID;
 
 timerButtons.addEventListener('click', event => {
     if (event.target.classList.contains('btn-timer')) {
         time = +event.target.getAttribute('data-time');
+        if (time < 60) {
+            timerPanel.innerHTML = `00:${time}`;
+        } else if ((time / 60) >= 10) {
+            timerPanel.innerHTML = `${Math.floor(time / 60)}:0${time % 60}`;
+        } else if ((time % 60) === 0) {
+            timerPanel.innerHTML = `0${Math.floor(time / 60)}:0${time % 60}`;
+        } else {
+            timerPanel.innerHTML = `0${Math.floor(time / 60)}:${time % 60}`;
+        }
     }
+
 });
 
 player.addEventListener('click', () => {
+    playMeditation();    
+});
+
+function playMeditation () {
     playBtn.classList.toggle('player-part-btn_hide');
     pauseBtn.classList.toggle('player-part-btn_hide');
-    setInterval(decreaseTime, 1000);
-});
+    timerStart();
+    timerStop();
+
+}
+
+function timerStart() {
+    if(playBtn.classList.contains('player-part-btn_hide')) {
+        timerID = setInterval(decreaseTime, 1000);
+    }
+    song.play();
+}
+
+function timerStop() {
+    if (pauseBtn.classList.contains('player-part-btn_hide')) {
+        clearInterval(timerID);
+        if (song.played) {
+            song.pause();
+        }
+    }
+}
+
 
 function decreaseTime() {
     let currentTime = --time;
@@ -42,6 +77,7 @@ function decreaseTime() {
 
 
 function stopPlay() {
+    song.pause();
     timerPanel.innerHTML = '00:00';//For test
 }
 
