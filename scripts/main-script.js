aa08a3e557969955a7d89c14e2bc7ca332cf335d
Kate <marri.nich@yandex.ru>
2021-11-29 15:55:47 +0300

    Added timer script. Keep going to work.

--- scripts/main-script.js
+++ scripts/main-script.js
@@ -1,6 +1,47 @@
 /*jshint esversion: 6 */
+const player = document.querySelector('.player-part');
+const playBtn = document.querySelector('#play-btn');
+const pauseBtn = document.querySelector('#pause-btn');
+const timerButtons = document.querySelector('#timer-buttons');
+const timerPanel = document.querySelector('#time-panel');
 
-const timeBtn = document.querySelector('.btn-timer_2');
-const durationTime = timeBtn.querySelector.data-time;
-console.log(durationTime);
+let time = 10;
+
+timerButtons.addEventListener('click', event => {
+    if (event.target.classList.contains('btn-timer')) {
+        time = +event.target.getAttribute('data-time');
+    }
+});
+
+player.addEventListener('click', () => {
+    playBtn.classList.toggle('player-part-btn_hide');
+    pauseBtn.classList.toggle('player-part-btn_hide');
+    setInterval(decreaseTime, 1000);
+});
+
+function decreaseTime() {
+    let currentTime = --time;
+    let leftMinutes = Math.floor(currentTime / 60);
+    let leftSeconds = Math.floor(currentTime % 60);
+
+    if (currentTime <= 0) {
+        stopPlay();
+    } else {
+        if (leftMinutes < 10) {
+            if (leftSeconds < 10) {
+                timerPanel.innerHTML = `0${leftMinutes}:0${leftSeconds}`;
+            } else {
+                timerPanel.innerHTML = `0${leftMinutes}:${leftSeconds}`;
+            }
+        } else {
+            timerPanel.innerHTML = `${leftMinutes}:${leftSeconds}`;
+        }
+    }
+}
+
+
+
+function stopPlay() {
+    timerPanel.innerHTML = '00:00';//For test
+}
 
